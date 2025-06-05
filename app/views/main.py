757bb42a1587c919167eb9c3cf215f42c101a1c5
jfnix <joaosantos1milion@gmail.com>
2025-06-04 23:16:38 -0300

    Fix blueprint path for React build

--- app/views/main.py
+++ app/views/main.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, send_from_directory
+import os
 
-views_bp = Blueprint('views', __name__)
+# Define o caminho absoluto da pasta 'build'
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build'))
 
-@views_bp.route('/')
-def index():
-    return render_template('index.html')
+# Cria o blueprint com a pasta estática correta
+views_bp = Blueprint('views', __name__, static_folder=root_dir)
+
+# Rota principal e fallback para SPA
+@views_bp.route('/', defaults={'path': ''})
+@views_bp.route('/<path:path>')
+def serve(path):
+    file_path = os.path.join(root_dir, path)
+
+    if path != "" and os.path.exists(file_path):
+        return send_from_directory(root_dir, path)
+    else:
+        return send_from_directory(root_dir, 'index.html')
